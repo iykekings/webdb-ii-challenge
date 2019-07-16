bebaff5600aec3cb0680d2b4ff2f9dd495ff8217
Ikechukwu <iykekings36@gmail.com>
2019-07-16 22:16:04 +0100

    Scaffold index.js with server

---
+++ index.js
@@ -0,0 +1,22 @@
+const knex = require('knex');
+const config = require('./knexfile.js');
+const express = require('express');
+
+const server = express();
+server.use(express.json());
+const db = knex(config.development);
+const getCars = id =>
+  id
+    ? db('cars')
+        .where({ id })
+        .first()
+    : db('cars');
+
+server.get('/', async (req, res) => {
+  const cars = await getCars();
+  res.status(200).json(cars);
+});
+
+server.listen(4000, () => {
+  console.log('Server running on port 4000');
+});
