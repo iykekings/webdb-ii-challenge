fea8cae55e7a5904f1b50c7460898780771d6f43
Ikechukwu <iykekings36@gmail.com>
2019-07-16 22:58:27 +0100

    implement update endpoint

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const express = require('express');
 const server = express();
 server.use(express.json());
 const db = knex(config.development);
+
+// db helpers
 const getCars = id =>
   id
     ? db('cars')
@@ -15,6 +17,14 @@ const createCar = car =>
   db('cars')
     .insert(car)
     .then(([id]) => getCars(id));
+
+const updateCar = (id, payload) =>
+  db('cars')
+    .where({ id })
+    .update(payload)
+    .then(res => getCars(id));
+
+// db helpers end
 
 server.get('/', async (req, res) => {
   const cars = await getCars();
@@ -34,6 +44,14 @@ server.post('/', async (req, res) => {
   res
     .status(400)
     .json({ message: 'Please provide VIN, model, make, mileage for the car' });
+});
+server.put('/:id', async (req, res) => {
+  const payload = req.body;
+  if (Object.keys(payload).length) {
+    const updatedCar = await updateCar(req.params.id, payload);
+    res.status(200).json(updatedCar);
+  }
+  res.status(400).json({ message: 'The update payload must not be empty' });
 });
 
 server.listen(4000, () => {
